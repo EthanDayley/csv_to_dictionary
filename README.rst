1babf4095834261c619be081e514b3c693db2ffe
Ethan Dayley <eedayley@gmail.com>
2018-01-10 20:52:12 -0700

    Initial commit, setup and package files.

---
+++ README.rst
@@ -0,0 +1,7 @@
+CSV to Dictionary Converter
+===========================
+I created this project after I discovered that one of my friends had abandoned python after being unable to find a simple and intuitive csv to dictionary converter.  I created this package in the hopes that I would not lose another friend from the pythonverse.
+:Authors
+    Ethan Dayley
+:Version 1.0
+:Dedication: To Jacob Adams, may he never stray again.
---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+The setup file for csv_to_dictionary
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+        name="csv_to_dictionary",
+        version="1.0",
+        description="Ultra simple csv to dictionary conversion library",
+        long_description=long_description,
+        url="https://github.com/EthanDayley/csv_to_dictionary",
+        author="Ethan Dayley",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3"
+        ],
+        keywords="simple csv dictionary parser"
+    )
